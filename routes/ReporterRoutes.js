87fc4188a44fc437f1ccbf40d65160da2e48cc12
Jonathan <81990209+Jonatndm@users.noreply.github.com>
2024-11-22 22:46:09 -0400

    cambio de endpoints

--- routes/ReporterRoutes.js
+++ routes/ReporterRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { obtenerPromedioEstudiante, obtenerPromedioEvaluacion, obtenerDesempenoMateria } = require('../Controllers/estudiantesController');
 const router = express.Router();
 
-router.get('/:id/promedioPorEstudiante', obtenerPromedioEstudiante);
-router.get('/:id/promedioPorEvaluacion', obtenerPromedioEvaluacion);
-router.get('/:id/desempenoPorMateria', obtenerDesempenoMateria);
+router.get('/promedioPorEstudiante/:id', obtenerPromedioEstudiante);
+router.get('/promedioPorEvaluacion/:id', obtenerPromedioEvaluacion);
+router.get('/desempenoPorMateria/:id', obtenerDesempenoMateria);
 
 
 module.exports = router;
